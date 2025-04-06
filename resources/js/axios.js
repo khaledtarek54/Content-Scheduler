badd04b5931f2cdff1924b888bc8d65aa4a66c8f
Khaled tarek <khaled.tarek@future-group.com>
2025-04-06 18:00:47 +0200

    axios

--- resources/js/axios.js
+++ resources/js/axios.js
@@ -3,8 +3,9 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000/api",
+    
 });
-
+console.log("Using API base URL:", instance.defaults.baseURL);
 const token = localStorage.getItem("token");
 if (token) {
     instance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
