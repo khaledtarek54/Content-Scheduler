fbaae1824a95f5c3ea00598e5b9ff42d19465d9b
Khaled tarek <khaled.tarek@future-group.com>
2025-04-07 18:06:54 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-cd /var/www/html
-npm run build
+# Set the image name
+IMAGE_NAME="khaledtarek54/content-scheduler:latest"
 
+# Pull the latest image from Docker Hub
+echo "Pulling the latest image from Docker Hub..."
+docker pull $IMAGE_NAME
+
+# Stop and remove the old container (if any)
+echo "Stopping and removing the old container..."
+docker stop content_scheduler_app
+docker rm content_scheduler_app
+
+# Run the new container
+echo "Running the new container..."
+docker run -d --name content_scheduler_app \
+  -p 80:80 \
+  --restart unless-stopped \
+  $IMAGE_NAME
